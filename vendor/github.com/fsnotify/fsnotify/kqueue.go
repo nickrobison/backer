b6909553d267bacbf77eddf2571b40c91622ea68
Nick Robison <nick@nickrobison.com>
2018-03-25 16:03:48 -0700

    Simple patch to prevent knotify from failing when debugging.

--- vendor/github.com/fsnotify/fsnotify/kqueue.go
+++ vendor/github.com/fsnotify/fsnotify/kqueue.go
@@ -505,6 +505,14 @@ func register(kq int, fds []int, flags int, fflags uint32) error {
 	return nil
 }
 
+// This is new
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 // read retrieves pending events, or waits until an event occurs.
 // A timeout of nil blocks indefinitely, while 0 polls the queue.
 func read(kq int, events []unix.Kevent_t, timeout *unix.Timespec) ([]unix.Kevent_t, error) {
@@ -512,6 +520,8 @@ func read(kq int, events []unix.Kevent_t, timeout *unix.Timespec) ([]unix.Kevent
 	if err != nil {
 		return nil, err
 	}
+	// This is new
+	n = min(n, len(events)-1)
 	return events[0:n], nil
 }
 
